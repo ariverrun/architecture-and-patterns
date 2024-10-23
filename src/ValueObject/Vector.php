c57b06349eef693a9fa374b29c7216716e53c6b6
Georgiy Grobovoy <gro-jorge@mail.ru>
2024-10-23 20:19:04 +0300

    Multy-dimensional vector with variadic constructor

--- src/ValueObject/Vector.php
+++ src/ValueObject/Vector.php
@@ -4,19 +4,34 @@ declare(strict_types=1);
 
 namespace App\ValueObject;
 
+use InvalidArgumentException;
+
 class Vector
 {
-    public function __construct(
-        protected readonly float $x,
-        protected readonly float $y,
-    ) {
+    /**
+     * @var float[]
+     */
+    private readonly array $values;
+
+    public function __construct(float ...$values)
+    {
+        $this->values = $values;
     }
 
     public static function plus(Vector $vectorA, Vector $vectorB): self
     {
-        return new Vector(
-            $vectorA->x + $vectorB->x,
-            $vectorA->y + $vectorB->y
-        );
+        if (count($vectorA->values) !== count($vectorB->values)) {
+            throw new InvalidArgumentException('Addition can not be performed for vectors with different dimensions amount');
+        }
+
+        $newValues = [];
+
+        foreach ($vectorA->values as $n => $aValue) {
+            $bValue = $vectorB->values[$n];
+
+            $newValues[] = $aValue + $bValue;
+        }
+
+        return new Vector(...$newValues);
     }
 }
