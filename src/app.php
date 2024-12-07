b704aaa7b7af2510a07f4a1d3627103e34d38bfb
Georgiy Grobovoy <gro-jorge@mail.ru>
2024-12-07 18:31:14 +0300

    Use custom php docker image (2)

--- src/app.php
+++ src/app.php
@@ -7,25 +7,3 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use \parallel\Runtime;
 
 var_dump(new Runtime());
-exit;
-
-use App\CommandQueue\CommandQueue;
-use App\CommandStrategy\DoubleRetryOnExceptionThenLogExceptionStrategy;
-use App\CommandStrategy\RetryOnExceptionThenLogExceptionStrategy;
-use Monolog\Level;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
-$queue = new CommandQueue();
-
-$logger = new Logger('default');
-$logger->pushHandler(new StreamHandler(__DIR__ . '/../var/log/error.log', Level::Error));
-
-$strategies = [
-    RetryOnExceptionThenLogExceptionStrategy::class,
-    DoubleRetryOnExceptionThenLogExceptionStrategy::class,
-];
-
-foreach ($strategies as $strategyClass) {
-    (new $strategyClass($queue, $logger))();
-}
