59b394a31ca3fa6545e73c6cb66aca09e81a5ed5
Georgiy Grobovoy <gro-jorge@mail.ru>
2024-12-07 17:56:31 +0300

    Added parallel in docker

--- src/app.php
+++ src/app.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use \parallel\Runtime;
+
+var_dump(new Runtime());
+exit;
+
 use App\CommandQueue\CommandQueue;
 use App\CommandStrategy\DoubleRetryOnExceptionThenLogExceptionStrategy;
 use App\CommandStrategy\RetryOnExceptionThenLogExceptionStrategy;
